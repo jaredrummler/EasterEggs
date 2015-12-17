2b6f1ab0f70e998295cc8542dca19874081b3c90
Jared Rummler <jared@jaredrummler.com>
2015-12-17 00:00:49 -0800

    simplify

--- beanbag/src/main/java/com/jaredrummler/android/eastereggs/beanbag/Board.java
+++ beanbag/src/main/java/com/jaredrummler/android/eastereggs/beanbag/Board.java
@@ -239,8 +239,7 @@ public class Board extends FrameLayout {
   public Board(Context context, AttributeSet as) {
     super(context, as);
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-      setSystemUiVisibility(0
-          | View.SYSTEM_UI_FLAG_FULLSCREEN
+      setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
           | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
           | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
           | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
